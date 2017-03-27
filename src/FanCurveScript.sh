08887f54428b26475e352cbab2184cc674d02b7f
sagir3 <smfahmid2009@gmail.com>
2017-03-26 21:06:55 -0400

    Updated NVIDIA user defined fan curve script
    Original script does not work anymore with the latest driver (378.13). I changed up some syntax according to this post: https://bbs.archlinux.org/viewtopic.php?pid=1527515#p1527515
    
    Potential improvement: a way to notify users in case the script becomes non-functional and fan speeds do not get changed.

--- src/FanCurveScript.sh
+++ src/FanCurveScript.sh
@@ -1,58 +1,70 @@
 #!/bin/bash
 #Make sure to enable nvidia-xconfig --cool-bits=4
-nvidia-settings -a "GPUFanControlState=1"
+
+
+#This new script works with version 378.13 driver
+#Old script is not functional (on Arch, at least)
+#Changed up some options and flags for nvidia-settings
+#Added some comments for clarity, in case other people want to make improved forks
+
+#Enable user defined fancontrol
+nvidia-settings -a "[gpu:0]/GPUFanControlState=1"
 
 while true
 do
 
+#Get GPU temperature
 gputemp=`nvidia-settings -q GPUCoreTemp |awk -F ":" 'NR==2{print $3}' |sed 's/[^0-9]*//g'`
 
+
+#Note: you need to set the minimum fan speed to a non-zero value, or it won't work
+#This fan profile is being used in my GTX580 (Fermi). Change it as necessary
+
+#If temperature is between X to Y degrees, set fanspeed to Z value
 case "${gputemp}" in
         0[0-9])
-                newfanspeed="0"
+                newfanspeed="30"
                 ;;
         1[0-9])
-                newfanspeed="0"
+                newfanspeed="30"
                 ;;
         2[0-9])
-                newfanspeed="0"
+                newfanspeed="30"
                 ;;
         3[0-9])
-                newfanspeed="0"
+                newfanspeed="30"
                 ;;
         4[0-9])
-                newfanspeed="0"
+                newfanspeed="30"
                 ;;
         5[0-4])
-                newfanspeed="0"
+                newfanspeed="40"
                 ;;
         5[5-6])
-                newfanspeed="5"
-                ;;
-        5[7-9])
-                newfanspeed="20"
-                ;;
-        6[0-5])
-                newfanspeed="35"
-                ;;
-        6[6-9])
                 newfanspeed="45"
                 ;;
-        7[0-5])
+        5[7-9])
+                newfanspeed="50"
+                ;;
+        6[0-5])
                 newfanspeed="60"
                 ;;
-        7[6-9])
+        6[6-9])
+                newfanspeed="65"
+                ;;
+        7[0-5])
                 newfanspeed="70"
+                ;;
+        7[6-9])
+                newfanspeed="85"
                 ;;
         *)
                 newfanspeed="100"
                 ;;
 esac
 
-nvidia-settings -a "GPUCurrentFanSpeed=${newfanspeed}" 2>&1 >/dev/null
+nvidia-settings -a "[fan-0]/GPUTargetFanSpeed=${newfanspeed}" 2>&1 >/dev/null
 
 sleep 1s
 
 done
-
-
