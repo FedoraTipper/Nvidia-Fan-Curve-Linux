5570c659b4f16063ae2d8e642cd4729104732784
Neo2SHYAlien <neo2shyalien@gmail.com>
2018-03-30 06:45:30 +0300

    add headless mode
    add headless mode
    add verbose output
    Signed-off-by: Neo2SHYAlien <neo2shyalien@gmail.com>

--- src/FanCurveScript.sh
+++ src/FanCurveScript.sh
@@ -7,6 +7,15 @@
 #Changed up some options and flags for nvidia-settings
 #Added some comments for clarity, in case other people want to make improved forks
 
+#enable headless mode 
+#request lightdm installed - apt install --no-install-recommends xorg lightdm for debian/ubuntu
+headless=true
+verbose=false
+
+if [ "$headless" = true ] ; then
+    export DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0
+fi
+
 #Enable user defined fancontrol for all gpu
 nvidia-settings -a "GPUFanControlState=1"
 
@@ -17,8 +26,11 @@ do
     i=0
 
     #Get GPU temperature of all cards
-    for gputemp in $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader);
-    do echo "gpu $i temp $gputemp"; 
+    for gputemp in $(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader);do
+    
+    if [ "$verbose" = true ] ; then
+        echo "gpu ${i} temp ${gputemp}"; 
+    fi
 
         #Note: you need to set the minimum fan speed to a non-zero value, or it won't work
         #This fan profile is being used in my GTX580 (Fermi). Change it as necessary
@@ -65,9 +77,13 @@ do
                         newfanspeed="100"
                         ;;
         esac
-
+        
         nvidia-settings -a "[fan-${i}]/GPUTargetFanSpeed=${newfanspeed}" 2>&1 >/dev/null
-
+        
+        if [ "$verbose" = true ] ; then
+            echo "gpu ${i} new fanspeed ${newfanspeed}"; 
+        fi
+        
         sleep 1s
     #increment gpu index
     i=$(($i+1))
